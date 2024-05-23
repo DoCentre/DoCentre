55baca6d10aad4bfe5df20d351f06beedd051bd5
Lai-YT <381xvmvbib@gmail.com>
2024-05-23 20:11:38 +0800

    refactor: use high-level smtp API

--- services/mail_service.go
+++ services/mail_service.go
@@ -1,9 +1,7 @@
 package services
 
 import (
-	"crypto/tls"
 	"fmt"
-	"log"
 	"net/smtp"
 	"os"
 
@@ -48,43 +46,13 @@ func sendEmailToApprover(documentID uint, approverID uint) error {
 		return err
 	}
 
-	addr := fmt.Sprintf("%s:%s", config.Host, config.Port)
-	log.Printf("Connecting to the remote SMTP server %s...\n", addr)
-	client, err := smtp.Dial(addr)
-	if err != nil {
-		return err
-	}
-
-	tlsConfig := tls.Config{
-		InsecureSkipVerify: true,
-		ServerName:         config.Host,
-	}
-	log.Println("Sending the STARTTLS command and encrypting all further communication...")
-	if err := client.StartTLS(&tlsConfig); err != nil {
-		return err
-	}
-	auth := smtp.PlainAuth("", config.AuthEmail, config.AuthPassword, config.Host)
-	if err := client.Auth(auth); err != nil {
-		return err
-	}
-
-	log.Println("Setting the sender and recipient...")
-	if err := client.Mail(config.SenderEmail); err != nil {
-		return err
-	}
+	// Prepare the necessary fields for sending an email.
 	rcpt, err := getEmailOf(approverID)
 	if err != nil {
 		return err
 	}
-	if err := client.Rcpt(rcpt); err != nil {
-		return err
-	}
-
-	log.Println("Setting the email content...")
-	wc, err := client.Data()
-	if err != nil {
-		return err
-	}
+	addr := fmt.Sprintf("%s:%s", config.Host, config.Port)
+	auth := smtp.PlainAuth("", config.AuthEmail, config.AuthPassword, config.Host)
 	subject := "[DoCentre] Document Approval Request"
 	header := fmt.Sprintf(
 		"To: %s\r\nFrom: %s\r\nSubject: %s\r\n\r\n",
@@ -92,18 +60,9 @@ func sendEmailToApprover(documentID uint, approverID uint) error {
 	contentTmpl := "You're assigned as an approver of the document %s, please review it."
 	url := fmt.Sprintf(config.DocUrlTmpl, documentID)
 	content := fmt.Sprintf(contentTmpl, url)
-	if _, err = fmt.Fprintf(wc, header+content); err != nil {
+	// Send the email.
+	if err := smtp.SendMail(addr, auth, config.SenderEmail, []string{rcpt}, []byte(header+content)); err != nil {
 		return err
 	}
-	if err := wc.Close(); err != nil {
-		return err
-	}
-
-	log.Println("Sending the QUIT command and closing the connection...")
-	if err := client.Quit(); err != nil {
-		return err
-	}
-
-	log.Println("Email sent successfully!")
 	return nil
 }
